08f14069e4d2f2e48259fb7d1267211cf2ee330c
MATHEUS OLIVEIRA <matheusoliver_123@hotmail.com>
2023-06-01 23:44:38 -0400

    Motificações

---
+++ ControleContatos/Controllers/ContasController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ControleContatos.Controllers
+{
+    public class ContasController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Criar()
+        {
+            return View();
+        }
+
+        public IActionResult Editar()
+        {
+            return View();
+        }
+
+        public IActionResult ApagarConta()
+        {
+            return View();
+        }
+    }
+}
